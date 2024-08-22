2cdaee77688ae55e79f4e8ffce25f7b9f2bea6a7
jucaza1 <jucaza@DESKTOP-H2PHB2K>
2024-08-22 23:48:41 +0200

    added some pluggins and remaps to telescope.buffers and snippets

--- lua/theprimeagen/lazy/mini.lua
+++ lua/theprimeagen/lazy/mini.lua
@@ -3,7 +3,7 @@ return {
     version = '*',
     config = function()
         require('mini.comment').setup() --gcc on normal, gc on visual, gc as textObject
-        require('mini.completion').setup()
+        -- require('mini.completion').setup()
         require('mini.jump').setup() --repeat f,F,t,T after f<letter>
         require('mini.pairs').setup()
         require('mini.surround').setup() --sa add, sr replace, sd delete, sf sF find r/l surrounding
--- lua/theprimeagen/lazy/telescope.lua
+++ lua/theprimeagen/lazy/telescope.lua
@@ -12,6 +12,7 @@ return {
 
         local builtin = require('telescope.builtin')
         vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+        vim.keymap.set('n', '<leader>pd', builtin.live_grep, {})
         vim.keymap.set('n', '<C-p>', builtin.git_files, {})
         vim.keymap.set('n', '<leader>pws', function()
             local word = vim.fn.expand("<cword>")
@@ -25,6 +26,31 @@ return {
             builtin.grep_string({ search = vim.fn.input("Grep > ") })
         end)
         vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
+        vim.keymap.set('n', '<leader>tr', builtin.treesitter, {})
+        local action_state = require('telescope.actions.state')
+        local actions = require('telescope.actions')
+
+        local buffer_searcher
+        buffer_searcher = function()
+            builtin.buffers {
+                sort_mru = true,
+                ignore_current_buffer = true,
+                show_all_buffers = false,
+                attach_mappings = function(prompt_bufnr, map)
+                    local refresh_buffer_searcher = function()
+                        actions.close(prompt_bufnr)
+                        vim.schedule(buffer_searcher)
+                    end
+                    local delete_buf = function()
+                        local selection = action_state.get_selected_entry()
+                        vim.api.nvim_buf_delete(selection.bufnr, { force = true })
+                        refresh_buffer_searcher()
+                    end
+                    map('n', 'dd', delete_buf)
+                    return true
+                end
+            }
+        end
+        vim.keymap.set('n', '<leader>vv', buffer_searcher)
     end
 }
-
