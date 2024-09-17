526418365896455d5a60a94e33e69af8dad2328f
jucaza1 <juan01cz@gmail.com>
2024-09-17 20:30:06 +0200

    fix: js debug now working, added respective plugins

--- lua/theprimeagen/lazy/dap.lua
+++ lua/theprimeagen/lazy/dap.lua
@@ -3,7 +3,15 @@ return {
         "mfussenegger/nvim-dap",
         dependencies = {
             "leoluz/nvim-dap-go",
-            'mfussenegger/nvim-dap-python',
+            "mfussenegger/nvim-dap-python",
+            "mxsdev/nvim-dap-vscode-js",
+            {
+                "microsoft/vscode-js-debug",
+                -- After install, build it and rename the dist directory to out
+                build =
+                "npm install --legacy-peer-deps --no-save && npx gulp vsDebugServerBundle && rm -rf out && mv dist out",
+                version = "1.*",
+            },
             "rcarriga/nvim-dap-ui",
             "theHamsta/nvim-dap-virtual-text",
             "nvim-neotest/nvim-nio",
@@ -33,40 +41,92 @@ return {
                 -- end,
             })
             require("dap-vscode-js").setup({
-                -- node_path = "node", -- Path of node executable. Defaults to $NODE_PATH, and then "node"
-                debugger_path = "~/.local/share/nvim/mason/packages/vscode-js-debug",
-                -- debugger_cmd = { "extension" }, -- Command to use to launch the debug server. Takes precedence over `node_path` and `debugger_path`.
-                adapters = { 'chrome', 'pwa-node', 'pwa-chrome', 'pwa-msedge', 'node-terminal', 'pwa-extensionHost', 'node', 'chrome' }, -- which adapters to register in nvim-dap
-                -- log_file_path = "(stdpath cache)/dap_vscode_js.log" -- Path for file logging
-                -- log_file_level = false -- Logging level for output to file. Set to false to disable file logging.
-                -- log_console_level = vim.log.levels.ERROR -- Logging level for output to console. Set to false to disable console output.
-            })
-            local js_based_languages = { "typescript", "javascript", "typescriptreact" }
+                -- Path of node executable. Defaults to $NODE_PATH, and then "node"
+                -- node_path = "node",
 
+                -- Path to vscode-js-debug installation.
+                debugger_path = vim.fn.resolve(vim.fn.stdpath("data") .. "/lazy/vscode-js-debug"),
+
+                -- Command to use to launch the debug server. Takes precedence over "node_path" and "debugger_path"
+                -- debugger_cmd = { "js-debug-adapter" },
+
+                -- which adapters to register in nvim-dap
+                adapters = {
+                    "chrome",
+                    "pwa-node",
+                    "pwa-chrome",
+                    "pwa-msedge",
+                    "pwa-extensionHost",
+                    "node-terminal",
+                },
+
+                -- Path for file logging
+                -- log_file_path = "(stdpath cache)/dap_vscode_js.log",
+
+                -- Logging level for output to file. Set to false to disable logging.
+                -- log_file_level = false,
+
+                -- Logging level for output to console. Set to false to disable console output.
+                -- log_console_level = vim.log.levels.ERROR,
+            })
+            local js_based_languages = {
+                "typescript",
+                "javascript",
+                "typescriptreact",
+                "javascriptreact",
+                "vue",
+            }
             for _, language in ipairs(js_based_languages) do
                 dap.configurations[language] = {
+                    -- Debug single nodejs files
                     {
                         type = "pwa-node",
                         request = "launch",
                         name = "Launch file",
                         program = "${file}",
-                        cwd = "${workspaceFolder}",
+                        cwd = vim.fn.getcwd(),
+                        sourceMaps = true,
                     },
+                    -- Debug nodejs processes (make sure to add --inspect when you run the process)
                     {
                         type = "pwa-node",
                         request = "attach",
                         name = "Attach",
-                        processId = require 'dap.utils'.pick_process,
-                        cwd = "${workspaceFolder}",
+                        processId = require("dap.utils").pick_process,
+                        cwd = vim.fn.getcwd(),
+                        sourceMaps = true,
                     },
+                    -- Debug web applications (client side)
                     {
                         type = "pwa-chrome",
                         request = "launch",
-                        name = "Start Chrome with \"localhost\"",
-                        url = "http://localhost:3000",
-                        webRoot = "${workspaceFolder}",
-                        userDataDir = "${workspaceFolder}/.vscode/vscode-chrome-debug-userdatadir"
-                    }
+                        name = "Launch & Debug Chrome",
+                        url = function()
+                            local co = coroutine.running()
+                            return coroutine.create(function()
+                                vim.ui.input({
+                                    prompt = "Enter URL: ",
+                                    default = "http://localhost:3000",
+                                }, function(url)
+                                    if url == nil or url == "" then
+                                        return
+                                    else
+                                        coroutine.resume(co, url)
+                                    end
+                                end)
+                            end)
+                        end,
+                        webRoot = vim.fn.getcwd(),
+                        protocol = "inspector",
+                        sourceMaps = true,
+                        userDataDir = false,
+                    },
+                    -- Divider for the launch.json derived configs
+                    {
+                        name = "----- ↓ launch.json configs ↓ -----",
+                        type = "",
+                        request = "launch",
+                    },
                 }
             end
             -- Handled by nvim-dap-go
